831ccbc44f39384cf569838c0bcca4be7ccd21da
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-26 11:54:40 -0400

    Update country model

--- src/app/models/country.model.ts
+++ src/app/models/country.model.ts
@@ -1,2 +1,35 @@
 export interface Country {
+    name: {
+        common: string;
+        official: string;
+        nativeName?: {
+            [key: string]: {
+                official: string;
+                common: string;
+            };
+        };
+    };
+    cca3: string;
+    flags: {
+        png: string;
+        svg: string;
+        alt?: string;
+    };
+    region: string;
+    subregion?: string;
+    capital?: string[];
+    population: number;
+    area?: number;
+    borders?: string[];
+    languages?: {
+        [key: string]: string;
+    };
+    currencies?: {
+        [code: string]: {
+            name: string;
+            symbol: string;
+        };
+    };
+    timezones?: string[];
+
 }
