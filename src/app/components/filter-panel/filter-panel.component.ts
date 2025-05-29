3e5d9bfe7142d10d66c7935e862fb4b7310cf39b
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-29 15:47:43 -0400

    Updated filter-panel code

--- src/app/components/filter-panel/filter-panel.component.ts
+++ src/app/components/filter-panel/filter-panel.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filter-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter-panel.component.html',
   styleUrl: './filter-panel.component.css'
 })
 export class FilterPanelComponent {
+
+  regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  @Output() regionSelected = new EventEmitter<string>();
+  @Input() isDisable = false
+
+  onSelect(value: any) {
+    this.regionSelected.emit(value);
+  }
 
 }
