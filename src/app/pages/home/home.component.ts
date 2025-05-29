202c3db640e681ac2a602423986052db4e4d2c38
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-29 15:51:41 -0400

    Added filter-panel, implements booleans variables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,17 @@ import { Country } from '../../models/country.model';
 import { CommonModule } from '@angular/common';
 import { CountryCardComponent } from '../../components/country-card/country-card.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { FilterPanelComponent } from '../../components/filter-panel/filter-panel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CountryCardComponent, SearchBarComponent],
+  imports: [
+    CommonModule,
+    CountryCardComponent,
+    SearchBarComponent,
+    FilterPanelComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -27,6 +33,8 @@ export class HomeComponent implements OnInit {
   itemsPerPage = 12;
 
   totalPages = 0;
+  disableSearchBar = false
+  disableFilterPanel = false
 
   loadAllCountries(): void {
     this.countryService.getAllCountries().subscribe((data) => {
@@ -49,21 +57,41 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  onSearch(value: any) {
+  onSearch(event: any) {
     this.currentPage = 1;
-    const term = value?.target?.value
+    const term = event?.target?.value
     const searchTerm = term.trim().toLowerCase();
 
     if (searchTerm != "") {
+      this.disableFilterPanel = true
       this.countryService.searchCountriesByName(searchTerm).subscribe((data) => {
         this.countries = data;
         this.totalPages = Math.ceil(this.countries.length / this.itemsPerPage);
         this.updatePagedCountries();
       });
     } else {
+      this.disableFilterPanel = false
       this.loadAllCountries()
     }
 
+  }
+
+  onSelect(event: any) {
+    this.currentPage = 1;
+    const region = event?.target?.value
+    const selecedRegion = region.trim().toLowerCase();
+
+    if (selecedRegion != "") {
+      this.disableSearchBar = true
+      this.countryService.getCountryByRegion(selecedRegion).subscribe((data) => {
+        this.countries = data;
+        this.totalPages = Math.ceil(this.countries.length / this.itemsPerPage);
+        this.updatePagedCountries();
+      });
+    } else {
+      this.disableSearchBar = false
+      this.loadAllCountries()
+    }
   }
 
 
