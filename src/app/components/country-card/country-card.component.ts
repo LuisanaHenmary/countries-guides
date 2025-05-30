2f4ea4367b2b6bcfb23da62f1ee5e92f960528e3
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-30 15:54:00 -0400

    Improved CountryCardComponent, added "More details" button

--- src/app/components/country-card/country-card.component.ts
+++ src/app/components/country-card/country-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Country } from '../../models/country.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-card.component.html',
   styleUrl: './country-card.component.css'
 })
